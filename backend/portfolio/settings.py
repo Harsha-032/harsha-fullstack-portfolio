196d8e01249fdd7766e7fe0cb5ada4d97f3852df
Harsha C <harshac032@gmail.com>
2026-06-07 21:40:10 +0530

    updated settings.py

--- backend/portfolio/settings.py
+++ backend/portfolio/settings.py
@@ -67,7 +67,7 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env_bool('DJANGO_DEBUG', False)
+DEBUG = env_bool('DJANGO_DEBUG', True)
 
 ALLOWED_HOSTS = env_list(
     'DJANGO_ALLOWED_HOSTS',
@@ -230,6 +230,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+WHITENOISE_USE_FINDERS = True
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
